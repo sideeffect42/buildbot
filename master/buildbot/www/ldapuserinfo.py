ef7ea12042be13cebfd3f5efed05722d960464eb
Takashi Yoshi <takashi@yoshi.email>
2018-04-09 19:50:45 +0200

    Make flake8 happy

--- master/buildbot/www/ldapuserinfo.py
+++ master/buildbot/www/ldapuserinfo.py
@@ -130,7 +130,7 @@ class LdapUserInfo(avatar.AvatarBase, auth.UserInfoProviderBase):
 
             def getLdapInfo(x):
                 if isinstance(x, list):
-                    x = x[0] if 0 < len(x) else None
+                    x = x[0] if x else None
                 return ldap_bytes2unicode(x)
 
             infos['full_name'] = getLdapInfo(ldap_infos[self.accountFullName])
